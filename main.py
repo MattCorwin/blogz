47aa48f62561931494b5583461891095dad32813
Matt Corwin <corwin.matt@gmail.com>
2018-11-05 20:28:30 -0600

    Added signup and login pages

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = "ssdf234223ljwe"
-"""
-class Task(db.Model):
+app.secret_key = "ssdf234223ljwe2"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
 
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
-"""
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60))
+    password = db.Column(db.String(60))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+#TODO FINISH THIS, ONLY COPIED IT
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
 
 @app.route("/blog")
 def index():
@@ -48,7 +57,7 @@ def create_new():
         if blog_title == "" or blog_body == "":
             flash("Please fill in both Title and Post fields", "error")
             return render_template('newpost.html', page_title="Add a blog entry", title=blog_title, body=blog_body)
-
+        #TODO ADD OWNER PARAMETER TO NEW POST CONSTRUCTOR BELOW
         new_post = Blog(blog_title, blog_body)
         db.session.add(new_post)
         db.session.commit()
@@ -58,33 +67,85 @@ def create_new():
     
     return render_template("newpost.html", page_title="Add a blog entry")
 
-"""@app.route('/', methods=['POST', 'GET'])
-def index():
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+        username = request.form['username']
+        password = request.form['password'] 
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['id'] = user.id
+            flash("Logged in")
+            return redirect('/newpost')
+        elif not user:
+            flash('That username does not exist, please retry or visit the signup page', 'error')
+            return redirect('/login')
+        elif user.password != password:
+            flash('Incorrect password')
+            return redirect('/login', username=username)
+
+    return render_template('login.html')
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        #returns welcome screen or shows any error messages
+        
+        username = request.form["username"]
+        password = request.form["password"]
+        password_retype = request.form["retype"]
+
+        duplicate = User.query.filter_by(username=username).first()
+    
+        #run through potential errors
+        if duplicate:
+            error_name = 'That username already exists, please enter a new one'
+            return render_template('signup.html', error_name=error_name)
+
+        if username.strip() == "":
+            error_name = "Please enter a Username"
+            return render_template("signup.html", error_name=error_name)
+    
+        if len(username) < 3:
+            error_name = "Please enter a Username with at least 3 characters"
+            return render_template("signup.html", error_name=error_name)
+
+        if password.strip() == "":
+            error_name = "Please enter a Password"
+            return render_template("signup.html", error_name=error_name, username=username)
+
+        if password_retype.strip() == "":
+            error_name = "Please retype your password"
+            return render_template("signup.html", error_name=error_name, username=username)
+
+        if password != password_retype:
+            error_name = "Original password and retype do not match, please retype passwords"
+            return render_template("signup.html", error_name=error_name, username=username)
+        
+        if len(password) < 3:
+            error_name = "Please enter a password with a length of at least 3 characters"
+            return render_template("signup.html", error_name=error_name, username=username)
+        
+        
+
+        new_user = User(username, password)
+        db.session.add(new_user)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
-"""
+        session['username'] = username
+        return redirect('/newpost')
 
-"""
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+    
+    return render_template("signup.html")
 
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
 
-    return redirect('/')
-"""
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/blog')
+
 
 if __name__ == '__main__':
     app.run()
